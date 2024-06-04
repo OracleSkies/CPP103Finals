2337ecda41ed752d89ef114162ae4e0fc4d3252e
Elisha <148520926+lofelei@users.noreply.github.com>
2024-06-04 08:47:52 +0800

    Update KingFE.py

--- Grocery Store Inventory System/KingFE.py
+++ Grocery Store Inventory System/KingFE.py
@@ -20,19 +20,32 @@ log_in_text = tk.Label(root, text="Log In", font=("Times",70,"bold"), fg="white"
 
 ###################################################
 
-
 User_Entry = tk.StringVar()
 Password_Entry = tk.StringVar()
 
+# username
 username_label = tk.Label(root, text="Username", font=("Roboto", 20), fg="white", bg="#000150").pack(pady=20)
 username_entry = tk.Entry(root, text= User_Entry, font=("Arial", 14), width=30, borderwidth=5).pack(pady=10)
 
-
+# passsword
 password_label = tk.Label(root, text="Password", font=("Roboto", 20), fg="white", bg="#00006E").pack(pady=20)
 password_entry = tk.Entry(root, text= Password_Entry, font=("Arial", 14), width=30, borderwidth=5).pack(pady=10)
 
+# login button
 login_button = tk.Button(root, text="Log In", font=("Roboto", 16), borderwidth=3)
 login_button.pack(pady=40)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 '''
