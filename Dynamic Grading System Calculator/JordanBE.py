e0ae69be00dd09a4ac839dd02f515fb9fcf091d6
Rens <laurencejohn0987654@gmail.com>
2024-06-18 22:38:30 +0800

    negromancer
    eto

--- Dynamic Grading System Calculator/JordanBE.py
+++ Dynamic Grading System Calculator/JordanBE.py
@@ -1,1 +1,27 @@
-#File para sa back end person 1
+import tkinter as tk
+from tkinter import ttk
+from tkinter import PhotoImage
+
+class WindowBackground:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Window Background")
+        self.root.geometry("1000x800")
+
+        bg_image_path = "window_background.png"
+        self.open_window = PhotoImage(file="window background.png")
+        
+        self.bg_label = ttk.Label(self.root, image=self.open_window)
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+class texts:
+    def __init__(self,root):
+        self.root = root
+    
+    def Text(self):
+       
+        home = ttk.Button.pack_forget()
+                
+      
+        entry = tk.Label(root, text = home,font = ("Signataria,20,Italic"),command = home    )
+
